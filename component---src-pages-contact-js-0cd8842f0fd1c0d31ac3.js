1f7f361161ce0adbd2420fe6d65a3ab99e8d307c
Ritesh Kumar <ritesh99rakesh@gmail.com>
2020-05-12 21:18:38 +0530

    Updates

---
+++ component---src-pages-contact-js-0cd8842f0fd1c0d31ac3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"Cuy+":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),c=n.n(a),l=n("Bl7J"),r=n("vrFN");t.default=function(){return c.a.createElement(l.a,null,c.a.createElement(r.a,{title:"Contact"}),c.a.createElement("h1",null,"Contact Me"))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-0cd8842f0fd1c0d31ac3.js.map
